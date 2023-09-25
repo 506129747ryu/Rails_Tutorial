54b160477463bd206af04a3241042975cf0d46e3
KeianRyu <k.ryu@saroute.co.jp>
2023-09-25 16:03:37 +0900

    add Test

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'foo/bar'
+  get 'foo/baz'
   get 'static_pages/home'
   get 'static_pages/help'
+  get  "static_pages/about"
+  get 'static_pages/contact'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
